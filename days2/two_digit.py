93ebe38d0787de3f0409a752356f3d488e6d41d1
Pipsson <juliusmallya8420@gmail.com>
2022-08-18 10:59:17 +0300

    body mass index end

---
+++ days2/two_digit.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num1 = int(two_digit_number[0]) # typecasting in python, str for string float=>float
+num2 = float(two_digit_number[1]) 
+
+print(num1 + num2)
+
